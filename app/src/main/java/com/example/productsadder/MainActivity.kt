7579de266832d859cc336ce449b8e786193439ad
priyesh777 <priyeshgautam1010@gmail.com>
2023-12-04 17:34:51 -0500

    Add updates for color picker

--- app/src/main/java/com/example/productsadder/MainActivity.kt
+++ app/src/main/java/com/example/productsadder/MainActivity.kt
@@ -1,18 +1,64 @@
 package com.example.productsadder
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.productsadder.databinding.ActivityMainBinding
+import com.skydoves.colorpickerview.ColorEnvelope
+import com.skydoves.colorpickerview.ColorPickerDialog
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var selectedImages = mutableListOf<Uri>()
+    private var selectedColors = mutableListOf<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.buttonColorPicker.setOnClickListener{
+            ColorPickerDialog.Builder(this)
+                .setTitle("Product Color")
+                .setPositiveButton("Select", object: ColorEnvelopeListener {
+                    override fun onColorSelected(envelope: ColorEnvelope?, fromUser: Boolean) {
+                        envelope?.let {
+                            selectedColors.add(it.color)
+                            updateColors()
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel") {colorPicker, _ ->
+                    colorPicker.dismiss()
+                }.show()
+        }
+
+        // FOR IMAGE ADDER
+//        val selectImagesActivityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+//            if(result.resultCode == RESULT_OK){
+//                val intent = result.data
+//            }
+//        }
+//
+//        binding.buttonImagesPicker.setOnClickListener{
+//            val intent = Intent(ACTION_GET_CONTENT)
+//            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+//            intent.type = "image/*"
+//            selectImagesActivityResult.launch(intent)
+//        }
+    }
+
+    private fun updateColors() {
+        var colors = ""
+        selectedColors.forEach{
+            colors = "$colors ${Integer.toHexString(it)}"
+        }
+        binding.tvSelectedColors.text = colors
     }
 
 
